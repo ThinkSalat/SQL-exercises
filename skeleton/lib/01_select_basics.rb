1714b573cad852ae08efc299213a3dfcfeac8816
Shawn Salat <Shawn.Salat@Gmail.com>
2018-04-30 14:02:42 -0700

    Complete sql select basics

--- skeleton/lib/01_select_basics.rb
+++ skeleton/lib/01_select_basics.rb
@@ -36,6 +36,11 @@ def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
   execute(<<-SQL)
+  SELECT name, (gdp/population) as per_capita_gdp
+  FROM 
+  countries
+  WHERE 
+  area > 5000000 
   SQL
 end
 
@@ -43,6 +48,11 @@ def small_and_wealthy
   # Show the name and continent of countries where the area is less than 2,000
   # and the gdp is more than 5,000,000,000.
   execute(<<-SQL)
+  SELECT name, continent
+  FROM 
+  countries
+  WHERE 
+  gdp > 5000000000 and area < 2000
   SQL
 end
 
@@ -50,12 +60,18 @@ def scandinavia
   # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
+  SELECT name, population 
+  FROM countries
+  WHERE name IN ('Denmark', 'Finland', 'Norway', 'Sweden')
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  where name LIKE 'G%'
   SQL
 end
 
@@ -64,5 +80,8 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+  SELECT name, area/1000 AS Area_in_KM
+  FROM countries
+  WHERE area BETWEEN 200000 and 250000
   SQL
 end
