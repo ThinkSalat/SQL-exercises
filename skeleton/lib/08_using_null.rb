de295c520fba21a021d8008ecbaebd26ef8d76f9
Shawn Salat <Shawn.Salat@Gmail.com>
2018-04-30 18:04:47 -0700

    Complete up to using null SQL exercises

--- skeleton/lib/08_using_null.rb
+++ skeleton/lib/08_using_null.rb
@@ -18,6 +18,9 @@ require_relative './sqlzoo.rb'
 def null_dept
   # List the teachers who have NULL for their department.
   execute(<<-SQL)
+    SELECT name
+    FROM teachers
+    WHERE dept_id IS NULL
   SQL
 end
 
@@ -25,6 +28,9 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+  SELECT teachers.name, depts.name
+  FROM teachers
+  LEFT OUTER JOIN depts ON teachers.dept_id = depts.id
   SQL
 end
 
@@ -33,6 +39,9 @@ def all_depts_join
   # NB: you can avoid RIGHT OUTER JOIN (and just use LEFT) by swapping
   # the FROM and JOIN tables.
   execute(<<-SQL)
+  SELECT teachers.name, depts.name
+  FROM teachers
+  RIGHT OUTER JOIN depts ON teachers.dept_id = depts.id
   SQL
 end
 
@@ -41,6 +50,9 @@ def teachers_and_mobiles
   # 444 2266' if no number is given. Show teacher name and mobile
   # #number or '07986 444 2266'
   execute(<<-SQL)
+  SELECT name, mobile
+  FROM teachers
+    COALESCE('07986 444 2266')
   SQL
 end
 
